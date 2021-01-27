90f7c418e32f47cbd312e9dbb4f4cd1b50acce72
Atemu <atemu.main@gmail.com>
2021-01-27 17:44:13 +0100

    zen-kernels: 5.10.10 -> 5.10.13

--- pkgs/os-specific/linux/kernel/linux-zen.nix
+++ pkgs/os-specific/linux/kernel/linux-zen.nix
@@ -1,7 +1,7 @@
 { lib, fetchFromGitHub, buildLinux, ... } @ args:
 
 let
-  version = "5.10.10";
+  version = "5.10.13";
   suffix = "zen1";
 in
 
@@ -14,7 +14,7 @@ buildLinux (args // {
     owner = "zen-kernel";
     repo = "zen-kernel";
     rev = "v${version}-${suffix}";
-    sha256 = "0jsi2q8k1w5zs5l6z1brm2mxpl9arv6n6linc8yj6xc75nydw6w4";
+    sha256 = "0a8lz1zyrdf3m97yigp9221xr7rzcdwgdvyipl732l5ix1y5hmx4";
   };
 
   extraMeta = {
