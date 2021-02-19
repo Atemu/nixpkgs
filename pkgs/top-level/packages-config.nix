cb607276da9232902b1822dac95dfd290391c960
Atemu <atemu.main@gmail.com>
2021-02-19 19:21:56 +0100

    packages-config: refactor and simplification
    Now inherits actual attrsets from super instead of mapping getAttr over a list
    of strings.
    
    Filtering isn't really needed as non-existent subsets should just be removed and
    recurseIntoAttrs can simply be mapAttr'd now.
    
    Some subsets have been removed because they no longer exist.

--- pkgs/top-level/packages-config.nix
+++ pkgs/top-level/packages-config.nix
@@ -6,38 +6,33 @@
   # Enable recursion into attribute sets that nix-env normally doesn't look into
   # so that we can get a more complete picture of the available packages for the
   # purposes of the index.
-  packageOverrides = super:
-  let
-    recurseIntoAttrs = sets:
-      super.lib.genAttrs
-        (builtins.filter (set: builtins.hasAttr set super) sets)
-        (set: super.recurseIntoAttrs (builtins.getAttr set super));
-  in recurseIntoAttrs [
-    "roundcubePlugins"
-    "emscriptenfastcompPackages"
-    "fdbPackages"
-    "nodePackages_latest"
-    "nodePackages"
-    "platformioPackages"
-    "haskellPackages"
-    "idrisPackages"
-    "sconsPackages"
-    "gns3Packages"
-    "quicklispPackagesClisp"
-    "quicklispPackagesSBCL"
-    "rPackages"
-    "apacheHttpdPackages_2_4"
-    "zabbix50"
-    "zabbix40"
-    "zabbix30"
-    "fusePackages"
-    "nvidiaPackages"
-    "sourceHanPackages"
-    "atomPackages"
-    "emacs26.pkgs"
-    "emacs27.pkgs"
-    "steamPackages"
-    "ut2004Packages"
-    "zeroadPackages"
-  ];
+  packageOverrides = super: with super; lib.mapAttrs (n: v: recurseIntoAttrs v) {
+    inherit
+      roundcubePlugins
+      fdbPackages
+      nodePackages_latest
+      nodePackages
+      platformioPackages
+      haskellPackages
+      idrisPackages
+      sconsPackages
+      gns3Packages
+      quicklispPackagesClisp
+      quicklispPackagesSBCL
+      rPackages
+      apacheHttpdPackages_2_4
+      zabbix50
+      zabbix40
+      zabbix30
+      fusePackages
+      sourceHanPackages
+      atomPackages
+      steamPackages
+      ut2004Packages
+      zeroadPackages
+    ;
+      # inherit (emacs) pkgs;
+      # inherit (emacs26) pkgs;
+      # inherit (emacs27) pkgs;
+  };
 }
