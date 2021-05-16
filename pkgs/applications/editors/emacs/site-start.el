0205e1f025211424a994579a75c665f08294182b
Atemu <atemu.main@gmail.com>
2021-05-16 11:33:38 +0200

    Revert "emacs: adapt to renamed native-comp variables"
    This reverts commit 67bbabc0a471ec286cf63f3424c91eb8b31d9000.
    
    Currently breaks emacsPgtkGcc because they haven't merged that change yet.

--- pkgs/applications/editors/emacs/site-start.el
+++ pkgs/applications/editors/emacs/site-start.el
@@ -47,11 +47,11 @@ least specific (the system profile)"
 ;;; Set up native-comp load path.
 (when (featurep 'comp)
   ;; Append native-comp subdirectories from `NIX_PROFILES'.
-  (setq native-comp-eln-load-path
+  (setq comp-eln-load-path
         (append (mapcar (lambda (profile-dir)
                           (concat profile-dir "/share/emacs/native-lisp/"))
                         (nix--profile-paths))
-                native-comp-eln-load-path)))
+                comp-eln-load-path)))
 
 ;;; Make `woman' find the man pages
 (defvar woman-manpath)
