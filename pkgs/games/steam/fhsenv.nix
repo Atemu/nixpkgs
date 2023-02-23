7e29baa499f4c1196987c22bb656f66a3e066a80
Atemu <atemu.main@gmail.com>
2023-02-23 12:51:54 +0100

    Revert "steam: add elfutils to fhsenv"
    This reverts commit 7e24562a4976e3a3e569946e452bb6feb5de12bb.

--- pkgs/games/steam/fhsenv.nix
+++ pkgs/games/steam/fhsenv.nix
@@ -148,9 +148,6 @@ in buildFHSUserEnv rec {
     xorg.libXxf86vm
     libelf
 
-    # pressure-vessel (required for mangohud and possibly more)
-    elfutils.out
-
     # Required
     glib
     gtk2
